cfed495e1f5b6a3cce5a72612bfca9ab22812f89
frankruge <frankruge@gmail.com>
2017-04-27 11:53:49 +0200

    print statements reduced to basic requirement

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,6 @@
+#! /usr/bin/env python3
+#from __future__ import division
 import mysql.connector
-import os
 import os
 import re
 import pysam
@@ -7,12 +8,13 @@ import pybedtools as pbt
 import numpy as np
 import subprocess
 import string
+import decimal
 
 os.chdir("/home/frank/Desktop/FH_Semester2/medizinischeGenomanalysepapinger/medizinische_genomanalysen_2017_assignment_1/")
 __author__ = 'Frank Ruge'
 pick_name='INS'
 #pick_name=raw_input("pick gene and press enter:")
-print(pick_name)
+
 annotation_file=''
 
 class Assignment1:
@@ -33,9 +35,9 @@ class Assignment1:
 
     
     def fetch_gene_coordinates(self, genome_reference, file_name):
-        #if the GENE was searched before, a file GENE.txt was generated in os.getcwd(). it contains the coordinates.
+        #if the GENE was searched before, a file GENE.txt was generated in os.getcwd(). it contains the coordinates. (TEST.TXT if you cloned the repository)
         # the entire UCSC part of this script is omitted if the file exists, thereby saving time. in order to run the
-        # UCSC part of this script, GENE.txt has to be removed.
+        # UCSC part of this script, GENE.txt or TEST.TXT has to be removed.
         if str(self.name + ".txt") in os.listdir(os.getcwd()):
             mygene=[]
             coords= open(str(self.name +  ".txt"), 'r')
@@ -44,14 +46,11 @@ class Assignment1:
                 a[i]=a[i].strip('\n')
             coords.close()
             mygene.append(a)
-            print("mygene")
-            print(mygene)
-
             if mygene[0]:
-                print(self.name+" has these coordinates: "+str(mygene[0][2].strip('\n'))+'\t'+str(mygene[0][3].strip('\n'))+'\t'+str(mygene[0][4].strip('\n'))+"\nYou have previously collected the coordinates for this gene\n")
+                #print(self.name+" has these coordinates: "+str(mygene[0][2].strip('\n'))+'\t'+str(mygene[0][3].strip('\n'))+'\t'+str(mygene[0][4].strip('\n'))+"\nYou have previously collected the coordinates for this gene\n")
                 return mygene
-            else:
-                pass
+            #else:
+             #   pass
 
         print("Connecting to UCSC to fetch data")
         #print(self.name)
@@ -119,15 +118,18 @@ class Assignment1:
         return mygene
 
     def get_sam_header(self):
-        #prints to stdout and saves to variable
+        self.bf = pysam.AlignmentFile(self.bamfile, 'rb')
         #header = pysam.view(self.bamfile, '-H') #is like samtools view -H file.bam
         header = self.bf.header #returns a two level dictionary with information
-        for list in header.items():
-            print(list[0]+'\t'+str([i for i in list[1]]))
+        #for list in header.items():
+        #    print(list[0]+'\t'+str([i for i in list[1]]))
+        self.bf.close()
+        for i in header:
+            print("header: "+str(header[i]))
         return(header)
 
     def get_properly_paired_reads_of_gene(self, outname, coordinates):
-        print('\nCOORDINATES '+str(coordinates))
+        self.bf = pysam.AlignmentFile(self.bamfile, 'rb')
         pairedreads= pysam.AlignmentFile(outname, 'wb', template=self.bf)
         count=0
 
@@ -136,53 +138,65 @@ class Assignment1:
                 pairedreads.write(read)
                 count+=1
         pairedreads.close()
-        print("\nNumber of proper pairs at " + self.name + " coordinates" + str(coordinates)+":\n" + str(count) + "\n\n")
+        #print("\nNumber of proper pairs at " + self.name + " coordinates" + str(coordinates)+":\n" + str(count) + "\n\n")
+        print("get_properly_paired_reads_of_gene: " + str(count))
+        self.bf.close()
         return
 
         
     def get_gene_reads_with_indels(self, coordinates):
+        self.bf = pysam.AlignmentFile(self.bamfile, 'rb')
         indels=[]
+        count=0
         for read in self.bf.fetch(reference=str(coordinates[0][2]).strip('chr'), start=int(coordinates[0][3]), end=int(coordinates[0][4])):
             if not read.is_unmapped:
                 cigar_line = read.cigartuples
 
                 for entry in cigar_line:
                     if len(cigar_line) > 1:
-                        indels.append(read)
-                        #print(cigar_line)
-        print("Number of indels found at the region " + self.name + " coordinates" + str(coordinates) +": \n" + str(len(indels)))
-        return(indels)
+                        #indels.append(read)
+                        count+=1
+        self.bf.close()
+        #print("Number of indels found at the region " + self.name + " coordinates" + str(coordinates) +": \n" + str(len(indels)))
+        print("get_gene_reads_with_indels: " + str(count))
+        return #indels
 
 
     def calculate_total_average_coverage(self, readlength):
         #number of reads * length of each read / size of the genome
-        count=0
+        self.bf = pysam.AlignmentFile(self.bamfile, 'rb')
+        counter=0
         genome=0
         #cov=self.bf.count_coverage() #pysam function has a bug. it is documented but has never been resolved
         for read in self.bf:
             if not read.is_unmapped:
-                count+=1
-
+                counter+=1
         #genome size = sum length of chromosomes
         for entry in self.bf.lengths:
-            genome=genome+entry
-        return((count*readlength)/genome)
+            genome = genome + entry
+        coverage = ((counter*readlength)/genome)
+        print("calculate_total_average_coverage "+str(round(coverage, 3)))
+        self.bf.close()
+        return
 
     def calculate_gene_average_coverage(self):
-        print("todo")
+        print("calculate_gene_average_coverage: todo")
         
     def get_number_mapped_reads(self):
+        self.bf = pysam.AlignmentFile(self.bamfile, 'rb')
         count=0
         for read in self.bf:
             if not read.is_unmapped:
                 count+=1
-        return(count)
+        print("get_number_mapped_reads: "+str(count))
+        self.bf.close()
+        return (count)
         
     def get_gene(self, symbol):
         gene_line=[]
         sym="'"+symbol+"'"
-        genes=self.genes
-        for line in genes:
+        self.genes = open("TEST.TXT", 'r')
+        for line in self.genes:
             if sym in line:
                 a = re.split(", ", line.strip('()').replace("'", ""))
                 if gene_line:                         #if gene_line has an entry
@@ -190,36 +204,49 @@ class Assignment1:
                         pass
                 else:
                     gene_line.append(a)
+        #print("get_gene: "+str(gene_line))
+        self.genes.close()
         return(gene_line)
 
     def get_gene_symbol(self, symbol):
+        #self.genes = open("TEST.TXT", 'r')
         gl = assignment1.get_gene(symbol)
-        return(gl[0][1])
+        print("get gene symbol: "+gl[0][0])
+        #print("get gene symbol: " + gl[0][5])
+        #print("get gene symbol: "+gl[0][6])
+        #print("get gene symbol: "+gl[0][7])
+        #self.genes.close()
+        return(gl[0][0])
 
         
     def get_region_of_gene(self, symbol):
+        #self.genes = open("TEST.TXT", 'r')
         gl = assignment1.get_gene(symbol)
-        coords=[gl[0][3], gl[0][4]]
-        return (coords)
+        coords=[gl[0][2],gl[0][3], gl[0][4]]
+        #coords = gl
+        print("get_region_of_gene" + str(coords))
+        #self.genes.close()
+        return coords
         
     def get_number_of_exons(self, symbol):
+
         gl = assignment1.get_gene(symbol)
-        exons = len(gl[0][7].strip(',').split(','))
+        exons = gl[0][6]
+        print("number of exons: " + str(exons))
         return exons
-        print("ads")
     
     def print_summary(self):
-        coordinates=tuple(assignment1.fetch_gene_coordinates("hg19", "human_genes.table")); print(coordinates) ##
-        header = assignment1.get_sam_header();print(header)
+        coordinates=tuple(assignment1.fetch_gene_coordinates("hg19", "human_genes.table")); #print(coordinates) ##
+        header = assignment1.get_sam_header();#print(header)
         #getting paired reads takes 2 min for example file
         proper_paired=assignment1.get_properly_paired_reads_of_gene(outname="paired.bam", coordinates=coordinates)
         indels=assignment1.get_gene_reads_with_indels(coordinates=coordinates)
-        cov=assignment1.calculate_total_average_coverage(readlength=100);print(cov)
-        mapped=assignment1.get_number_mapped_reads();print(mapped)
-
-        #gene_symbol=assignment1.get_gene_symbol(symbol="INS");print(gene_symbol)
-        # region=assignment1.get_region_of_gene("INS");print(region)
-        #exons = assignment1.get_number_of_exons("INS");print(exons)
+        cov=assignment1.calculate_total_average_coverage(readlength=100);#print(cov)
+        assignment1.calculate_gene_average_coverage()
+        mapped=assignment1.get_number_mapped_reads();#print(mapped)
+        gene_symbol=assignment1.get_gene_symbol(symbol=self.name)#"INS");#print(gene_symbol)
+        region=assignment1.get_region_of_gene(symbol=self.name);#print(region)
+        exons = assignment1.get_number_of_exons(symbol=self.name);#print(exons)
     
         
 if __name__ == '__main__':
